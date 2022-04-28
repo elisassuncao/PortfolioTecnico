bf52f7f8f15e81f9702f8de8815fafe40b64c264
elisassuncao <83764786+elisassuncao@users.noreply.github.com>
2022-04-28 10:42:00 -0300

    Create ProjetoPessoaRoupa.java

---
+++ Modelagem de Sistemas/ProjetoPessoaRoupa.java
@@ -0,0 +1,44 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Tudo certinho!");
+  }
+}
+
+public class Pessoa{
+  private String nome;
+  private int idade;
+  private Roupa roupa;
+
+  public Pessoa(String n, int i){
+    nome = n;
+    idade = i;
+  }
+  
+  public void  andar(int i){
+    
+  }
+
+  public String get_nome(){
+    return nome;
+  }
+}
+
+public class Roupa{
+  private String tamanho;
+  private String cor;
+  private String material;
+
+  public Roupa(String t, String c, String m){
+    tamanho = t;
+    cor = c;
+    material = m;
+  }
+  
+  public boolean ajustar(String t){
+    return true;
+  }
+  
+  public void vestir(){
+    
+  }
+}
